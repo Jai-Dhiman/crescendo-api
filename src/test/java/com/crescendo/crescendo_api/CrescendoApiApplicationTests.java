4c73d5f3042d6d7fb982dcd16ee4c4b8e5678ad2
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-15 18:27:27 -0800

    init

---
+++ src/test/java/com/crescendo/crescendo_api/CrescendoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crescendo.crescendo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescendoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
